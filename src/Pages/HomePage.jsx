b84a76ad432da55e037ddc0b01512d59df27af02
fabiobosa <fabiobosar@gmail.com>
2024-09-15 11:53:51 -0300

    add HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,33 @@
 import React from 'react'
 import { Box } from '@mui/material'
+import banner_header_1 from '../assets/imgs/Home/Header/banner_header.webp'
+import banner_header_2 from '../assets/imgs/Home/Header/banner_header_b.webp'
+import banner_header_3 from '../assets/imgs/Home/Header/banner_header_c.webp'
+import Header from '../Components/Header'
+
+const headerProps = {
+  element1: {
+    image: banner_header_1,
+    title: 'Casa completa com piscina',
+    subTitle:
+      'Desfrute de bons momentos com sua família ou amigos em um espaço almplo e aconchegante',
+    buttonText: 'Conhecer alojamento'
+  },
+  element2: {
+    image: banner_header_2,
+    title: 'Casa completa na beira mar',
+    subTitle:
+      'Aproveite todo o conforte de uma casa confortável e tire dias de descanso com vista para o mar',
+    buttonText: 'Conhecer alojamento'
+  },
+  element3: {
+    image: banner_header_3,
+    title: 'Casa confortável próximo a praia',
+    subTitle:
+      'Aproveite todo o conforto de uma casa confortável e tire dias de descanso a poucos metros do mar',
+    buttonText: 'Conhecer alojamento'
+  }
+}
 
 const HomePage = () => {
   return (
@@ -10,6 +38,7 @@ const HomePage = () => {
         width: '100%'
       }}
     >
+      <Header headerProps={headerProps} />
     </Box>
   )
 }
