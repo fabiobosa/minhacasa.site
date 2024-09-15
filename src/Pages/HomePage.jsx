29894827a5bd49226fab2aeb17820588d4ab6e2d
fabiobosa <fabiobosar@gmail.com>
2024-09-15 11:43:40 -0300

    add HomePage

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+const HomePage = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%'
+      }}
+    >
+    </Box>
+  )
+}
+
+export default HomePage
