0d05194a315c47fd55ad38c5672dd94273dbce36
fabiobosa <fabiobosar@gmail.com>
2024-09-15 12:29:54 -0300

    add GenericFields

---
+++ src/Components/GenericFieldButton.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+import Button1 from '../Components/Buttons/Button1'
+
+const GenericFieldButton = ({ imgSrc, text, buttonText }) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: { xs: 'center', md: 'flex-start' },
+        gap: 2,
+        width: '100%',
+        minHeight: { xs: 'auto', md: '507px' }
+      }}
+    >
+      <Box
+        component="img"
+        src={imgSrc}
+        alt="Descrição da imagem"
+        sx={{
+          width: { xs: '100%', md: '100%' },
+          height: 'auto',
+          maxWidth: '100%'
+        }}
+      />
+      <Typography
+        variant="body1"
+        sx={{
+          textAlign: { xs: 'left', md: 'left' }
+        }}
+      >
+        {text}
+      </Typography>
+
+      <Button1 text={buttonText} />
+    </Box>
+  )
+}
+
+export default GenericFieldButton
---
+++ src/Components/GenericFieldTitle.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+export const GenericFieldTitle = ({ imgSrc, title, text }) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: { xs: 'center', md: 'flex-start' },
+        gap: 2,
+        width: '100%',
+        minHeight: { xs: 'auto', md: '507px' }
+      }}
+    >
+      <Typography
+        variant="h2"
+        sx={{
+          textAlign: { xs: 'left', md: 'left' }
+        }}
+      >
+        {title}
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{
+          textAlign: { xs: 'left', md: 'left' }
+        }}
+      >
+        {text}
+      </Typography>
+
+      <Box
+        component="img"
+        src={imgSrc}
+        alt="Descrição da imagem"
+        sx={{
+          width: { xs: '100%', md: '100' },
+          height: 'auto',
+          maxWidth: '100%'
+        }}
+      />
+    </Box>
+  )
+}
+
+export default GenericFieldTitle
